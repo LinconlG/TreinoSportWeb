b62adc1686bccdb3c2df29b190f0e3bbd4ebcbad
Linconl <67241771+LinconlG@users.noreply.github.com>
2025-05-31 13:53:01 -0300

    ajuste modal

--- src/app/components/modals/criar-treino/criar-treino.component.ts
+++ src/app/components/modals/criar-treino/criar-treino.component.ts
@@ -7,16 +7,40 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { timer } from 'rxjs';
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { Modalidade } from '../../../shared/enums/modalidade';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @Component({
   selector: 'app-criar-treino',
-  imports: [CommonModule, ReactiveFormsModule, MatDialogModule, MatInputModule, MatButtonModule, MatFormFieldModule],
+  imports: [CommonModule, ReactiveFormsModule, MatDialogModule, MatInputModule, MatButtonModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './criar-treino.component.html',
   styleUrl: './criar-treino.component.css'
 })
 export class CriarTreinoComponent {
   form: FormGroup;
+  Modalidade = Modalidade;
+  modalidades = Object.values(Modalidade);
+  selectedModalidade: Modalidade | null = null;
+
+  getNomeModalidade(modalidade: Modalidade): string {
+    const nomes = {
+      [Modalidade.BEACHTENNIS]: 'Beach Tennis',
+      [Modalidade.DANCA]: 'Dança',
+      [Modalidade.FUNCIONAL]: 'Funcional',
+      [Modalidade.FUTEBOL]: 'Futebol',
+      [Modalidade.FUTSAL]: 'Futsal',
+      [Modalidade.FUTVOLEI]: 'Futvôlei',
+      [Modalidade.GINASTICA]: 'Ginástica',
+      [Modalidade.JIUJITSU]: 'Jiu Jitsu',
+      [Modalidade.MUAITHAI]: 'Muay Thai',
+      [Modalidade.NATAÇÃO]: 'Natação',
+      [Modalidade.PILATES]: 'Pilates',
+      [Modalidade.TENIS]: 'Tênis',
+      [Modalidade.VOLEI]: 'Vôlei'
+    };
+    return nomes[modalidade] || modalidade;
+  }
 
   constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<CriarTreinoComponent>, private overlayContainer: OverlayContainer) {
     this.form = this.fb.group({
