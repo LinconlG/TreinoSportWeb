7837d63dceea05a96892d2d59cd156ab9568dc51
Linconl <67241771+LinconlG@users.noreply.github.com>
2025-07-21 11:35:19 -0300

    fix: corrigido erro ao atualizar pagina

--- src/app/components/gerenciamento-treino/gerenciamento-treino.component.ts
+++ src/app/components/gerenciamento-treino/gerenciamento-treino.component.ts
@@ -10,8 +10,14 @@ import { Router } from '@angular/router';
 export class GerenciamentoTreinoComponent {
   codigoTreino: number
   constructor(private router:Router){
-    const nav = this.router.getCurrentNavigation();
-    this.codigoTreino = nav.extras.state?.['data'];
-    console.log(this.codigoTreino);
+    try{
+      const nav = this.router.getCurrentNavigation();
+      this.codigoTreino = nav.extras.state?.['data'];
+      console.log(this.codigoTreino);
+    } catch{
+      this.router.navigate(['/home/ct']);
+    }
   }
+
+
 }
